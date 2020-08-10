f3123a58811e692cee664556a79d2630415c3422
Jos van Olst <vanolstj@gmail.com>
2020-08-10 16:40:38 +1200

    Add read file function
    Added functionality for read_file function that returns all lines from a given file

--- get_filenames.py
+++ get_filenames.py
@@ -2,6 +2,7 @@ import glob
 import os
 
 def get_filenames(path):
+    '''Returns a list of all javascript files recursively from a given path'''
     filenames = []
 
     for folder in os.walk(path):
@@ -10,5 +11,23 @@ def get_filenames(path):
 
     return filenames
 
+def read_file(path):
+    '''Returns a list of all lines in a specified file'''
+    try:
+        file = open(path, 'r')
+        lines = file.read().split('\n')
+        file.close()
+    except (OSError, IOError) as e:
+        print(f'File: {os.path.join(os.path.abspath(path))} not found.')
+        lines = []
+
+    return lines
+
 print(get_filenames('sample-es6/src'))
 
+filenames = get_filenames('sample-es6/src')
+read_file('error')
+lines = read_file(filenames[0])
+
+for line in lines:
+    print(line)
